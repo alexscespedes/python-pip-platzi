9e7f7341b7d150b4d81893562815819dcf70b1f0
alexscespedes <alexandersenc99@gmail.com>
2023-08-04 16:27:21 -0400

    update dependencies http-pandas-fastapi

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+
+    df = pd.read_csv('data.csv')
+    df = df[df['Continent'] == 'Africa']
+
+    countries = df['Country/Territory'].values
+    percentages = df['World Population Percentage'].values
+    charts.generate_pie_chart(countries, percentages)
+
+
     data = read_csv.read_csv('data.csv')
     country = input('Type Country => ')
     print(country)
